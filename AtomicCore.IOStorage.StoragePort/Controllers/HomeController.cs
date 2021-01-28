01abf7d948977f07dcebea0a9614e8bc4c7cd946
dannyhu <171331668@qq.com>
2021-01-28 15:59:55 +0800

    2

--- AtomicCore.IOStorage.StoragePort/Controllers/HomeController.cs
+++ AtomicCore.IOStorage.StoragePort/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace AtomicCore.IOStorage.StoragePort.Controllers
     {
         public IActionResult Index()
         {
-            return Content("<a href='/BizIOStorageProvider.svc' target='_blank'>查看服务地址</a>");
+            return View();
         }
     }
 }

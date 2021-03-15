8e4114808f1a84ccff05ce5413da709ee751445d
danny <171331668@qq.com>
2021-03-15 17:24:48 +0800

    add unitTest

---
+++ AtomicCore.UnitTest/Extensions/BufferExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.Tests
+{
+    [TestClass()]
+    public class BufferExtensionsTests
+    {
+        [TestMethod()]
+        public void ToBytesTest()
+        {
+            Uri uri = new Uri("http://www.baidu.com");
+
+            byte[] buffer = uri.ToBytes();
+
+            Uri secUri = buffer.ToObject<Uri>();
+
+            Assert.IsTrue(null != buffer);
+        }
+    }
+}

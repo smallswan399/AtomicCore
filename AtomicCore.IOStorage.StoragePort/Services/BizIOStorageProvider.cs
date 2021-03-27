3a1d6e3a9bfacc55c9eda13e1bec3aaff02a4cd1
danny <171331668@qq.com>
2021-03-27 13:40:09 +0800

    2

---
+++ AtomicCore.IOStorage.StoragePort/Services/BizIOStorageProvider.cs
@@ -0,0 +1,44 @@
+﻿using AtomicCore.IOStorage.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtomicCore.IOStorage.StoragePort
+{
+    /// <summary>
+    /// IO服务接口
+    /// https://lebang2020.cn/details/210110njneqn2f.html
+    /// </summary>
+    public class BizIOStorageProvider : IBizIOStorageProvider
+    {
+        public BizIOOnputBreakPointTrans BreakPointTransmition(BizIOInputBreakPointTrans inputParam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BizIOOutputDelete Delete(BizIOInputDelete inputParam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BizIOOutputDirectory GetDirectory(BizIOInputDirectory inputParam)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 回路测试
+        /// </summary>
+        /// <returns></returns>
+        public string LoopTester()
+        {
+            return string.Format(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        public BizIOOutputSave Save(BizIOInputSave inputParam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

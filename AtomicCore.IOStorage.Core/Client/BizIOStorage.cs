3ae0a4b185556a5c92b4fb2278633183c5663934
danny <171331668@qq.com>
2021-04-19 21:03:14 +0800

    add BizIOStorageClient

---
+++ AtomicCore.IOStorage.Core/Client/BizIOStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.IOStorage.Core
+{
+    /// <summary>
+    /// IO存储客户端调用服务类
+    /// </summary>
+    public static class BizIOStorageClient
+    {
+    }
+}

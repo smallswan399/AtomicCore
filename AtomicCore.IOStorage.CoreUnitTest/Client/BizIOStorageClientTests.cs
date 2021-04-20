9f50ef2aa5a66b20aeba576c2d1fc5c0e0bead12
danny <171331668@qq.com>
2021-04-20 14:08:54 +0800

    add unit test

---
+++ AtomicCore.IOStorage.CoreUnitTest/Client/BizIOStorageClientTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore.IOStorage.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AtomicCore.IOStorage.Core.Tests
+{
+    [TestClass()]
+    public class BizIOStorageClientTests
+    {
+        static BizIOStorageClientTests()
+        {
+            AtomicCore.AtomicKernel.Initialize();
+        }
+
+        [TestMethod()]
+        public void UploadFileTest()
+        {
+            string basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+            string path = string.Format("{0}test.jpg", basePath);
+
+            BizIOSingleUploadJsonResult result;
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                fs.Seek(0, SeekOrigin.Begin);
+
+                BizIOStorageClient client = new BizIOStorageClient("http://192.168.0.11:8777");
+                result = client.UploadFile(new BizIOUploadFileInput()
+                {
+                    BizFolder = "Test",
+                    FileStream = fs
+                });
+            }
+
+            Assert.IsTrue(null != result && result.Code == BizIOStateCode.Success);
+        }
+    }
+}

96282b3666d61669ef82538f7d9fecd4dd438859
danny <171331668@qq.com>
2021-06-03 13:22:56 +0800

    add socket emit

---
+++ AtomicCore.SocketIO.Emitter/EmitterOptions.cs
@@ -0,0 +1,33 @@
+﻿namespace AtomicCore.SocketIO.Emitter
+{
+    /// <summary>
+    /// Emitter Options
+    /// </summary>
+    public class EmitterOptions
+    {
+        /// <summary>
+        /// 主机IP
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// 端口
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// KEY
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 请求版本
+        /// </summary>
+		public EVersion Version = EVersion.V0_9_9;
+
+        /// <summary>
+        /// EV版本
+        /// </summary>
+        public enum EVersion { V0_9_9, V1_4_4 };
+    }
+}
---
+++ AtomicCore.SocketIO.Emitter/Flag.cs
@@ -0,0 +1,13 @@
+﻿namespace AtomicCore.SocketIO.Emitter
+{
+    /// <summary>
+    /// 标识位
+    /// </summary>
+    public class Flags
+    {
+        public static string JSON = "json";
+        public static string Volatile = "volatile";
+        public static string Broadcast = "broadcast";
+        public static string Binary = "binary";
+    }
+}

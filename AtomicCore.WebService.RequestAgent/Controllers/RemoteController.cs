0e1e77379804735f7b4bf857466655a007b4487b
adu <adu@adu.com>
2021-07-13 11:32:29 +0800

    adds

--- AtomicCore.WebService.RequestAgent/Controllers/RemoteController.cs
+++ AtomicCore.WebService.RequestAgent/Controllers/RemoteController.cs
@@ -17,16 +17,34 @@ namespace AtomicCore.WebService.RequestAgent.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
-            return NoContent();
+            return Content(DateTime.Now.ToString());
         }
-        
+
         /// <summary>
         /// Get请求
         /// </summary>
+        /// <param name="url">请求URL</param>
         /// <returns></returns>
-        public IActionResult Get()
+        public IActionResult Get(string url)
         {
-            return null;
+            if (string.IsNullOrEmpty(url))
+                return Content("url null");
+
+            string get_url = System.Web.HttpUtility.UrlDecode(url);
+
+            Dictionary<string, string> get_heads = this.Request.Headers.ToDictionary(k => k.Key, v => v.Value.ToString());
+
+            string res;
+            try
+            {
+                res = HttpProtocol.HttpGet(get_url, string.Empty, get_heads, null);
+            }
+            catch (Exception ex)
+            {
+                return Content(null == ex.InnerException ? ex.Message : ex.InnerException.Message);
+            }
+
+            return Content(res);
         }
     }
 }

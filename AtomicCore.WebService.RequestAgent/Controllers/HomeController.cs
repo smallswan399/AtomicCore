25c5318302d305e179dce442a88f1b05ee5f5aa5
adu <adu@adu.com>
2021-07-13 11:46:07 +0800

    adds

--- AtomicCore.WebService.RequestAgent/Controllers/HomeController.cs
+++ AtomicCore.WebService.RequestAgent/Controllers/HomeController.cs
@@ -14,6 +14,8 @@ namespace AtomicCore.WebService.RequestAgent.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
+            //string encodeUrl = System.Web.HttpUtility.UrlEncode("https://api-goerli.etherscan.io/api?module=account&action=balance&address=0x29aAe16abfDC4C6F119E86D09ab8603D491c5d5F&tag=latest&apikey=JD5TN6TN7KVGW21N7Q9ETH7FSIUTD4W4S2");
+
             return Content("welcome to io request agent port...");
         }
     }
--- AtomicCore.WebService.RequestAgent/Controllers/RemoteController.cs
+++ AtomicCore.WebService.RequestAgent/Controllers/RemoteController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AtomicCore.WebService.RequestAgent.Controllers
 {
@@ -32,12 +30,39 @@ namespace AtomicCore.WebService.RequestAgent.Controllers
 
             string get_url = System.Web.HttpUtility.UrlDecode(url);
 
-            Dictionary<string, string> get_heads = this.Request.Headers.ToDictionary(k => k.Key, v => v.Value.ToString());
+            string res;
+            try
+            {
+                res = HttpProtocol.HttpGet(get_url, string.Empty, null, null);
+            }
+            catch (Exception ex)
+            {
+                return Content(null == ex.InnerException ? ex.Message : ex.InnerException.Message);
+            }
+
+            return Content(res);
+        }
+
+        /// <summary>
+        /// POST请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
+        public IActionResult Post(string url, string contentType = HttpProtocol.APPLICATIONJSON)
+        {
+            if (string.IsNullOrEmpty(url))
+                return Content("url null");
+
+            string post_url = System.Web.HttpUtility.UrlDecode(url);
+            string post_date = null;
+            if (this.Request.Form.Count > 0)
+                post_date = string.Join("&", this.Request.Form.Select(s => string.Format("{0}={1}", s.Key, s.Value.ToString())));
 
             string res;
             try
             {
-                res = HttpProtocol.HttpGet(get_url, string.Empty, get_heads, null);
+                res = HttpProtocol.HttpPost(post_url, post_date, contentType, null, null);
             }
             catch (Exception ex)
             {

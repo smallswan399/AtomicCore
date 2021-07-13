5f1f1ec08f7b7d60fb570e23f13d59aa20269b61
adu <adu@adu.com>
2021-07-13 15:40:10 +0800

    adds

--- AtomicCore.IOStorage.CoreUnitTest/Infrastructure/Protocol/UrlEncoderTests.cs
+++ AtomicCore.IOStorage.CoreUnitTest/Infrastructure/Protocol/UrlEncoderTests.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AtomicCore.Infrastructure.Protocol;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AtomicCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AtomicCore.Infrastructure.Protocol.Tests
+namespace AtomicCore.Tests
 {
     [TestClass()]
     public class UrlEncoderTests
@@ -17,6 +12,16 @@ namespace AtomicCore.Infrastructure.Protocol.Tests
             var str = UrlEncoder.UrlEncode("https://api-goerli.etherscan.io/api?module=account&action=balance&apikey=N4KR7R89K78AWKPBZY6WD27DDTTDB8YJ8W&address=0x29aAe16abfDC4C6F119E86D09ab8603D491c5d5F&tag=latest");
 
             Assert.IsTrue(!str.Contains("&"));
+        }
+
+        [TestMethod()]
+        public void UrlDecodeTest()
+        {
+            var str = UrlEncoder.UrlEncode("https://api-goerli.etherscan.io/api?module=account&action=balance&apikey=N4KR7R89K78AWKPBZY6WD27DDTTDB8YJ8W&address=0x29aAe16abfDC4C6F119E86D09ab8603D491c5d5F&tag=latest");
+
+            string newStr = UrlEncoder.UrlDecode(str);
+
+            Assert.IsTrue(newStr.Contains("&"));
         }
     }
 }

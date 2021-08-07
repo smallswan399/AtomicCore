3d9407008140bde10af70c7eaf963457b9058b7d
adu <adu@adu.com>
2021-08-07 15:55:19 +0800

    adds

---
+++ AtomicCore.BlockChain.TronscanAPI/TronScanClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.BlockChain.TronscanAPI
+{
+    /// <summary>
+    /// TRON SCAN CLIENT
+    /// </summary>
+    public class TronScanClient : ITronScanClient
+    {
+    }
+}

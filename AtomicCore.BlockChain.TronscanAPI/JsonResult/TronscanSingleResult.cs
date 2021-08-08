074be73c5ad1d2badb7518534c34e3a1dba1ab2f
adu <adu@adu.com>
2021-08-08 20:22:18 +0800

    adds

---
+++ AtomicCore.BlockChain.TronscanAPI/JsonResult/TronscanSingleResult.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace AtomicCore.BlockChain.TronscanAPI
+{
+    /// <summary>
+    /// Tronscan Single Result
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class TronscanSingleResult<T> : TronscanResultBase
+    {
+        /// <summary>
+        /// 数据结果
+        /// </summary>
+        [JsonProperty("data")]
+        public T Result { get; set; }
+    }
+}

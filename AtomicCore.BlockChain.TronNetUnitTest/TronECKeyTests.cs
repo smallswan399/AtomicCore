81b22f2a15d20fb66eaf9260531845e74804ea2a
adu <adu@adu.com>
2021-08-16 18:52:37 +0800

    adds test

---
+++ AtomicCore.BlockChain.TronNetUnitTest/TronECKeyTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore.BlockChain.TronNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.BlockChain.TronNet.Tests
+{
+    [TestClass()]
+    public class TronECKeyTests
+    {
+        [TestMethod()]
+        public void GenerateKeyTest()
+        {
+            var privateKey = TronTestAccountCollection.TestMain.PirvateKey;
+
+            TronECKey mainKey = new TronECKey(privateKey, TronNetwork.MainNet);
+            string address = mainKey.GetPublicAddress();
+
+            Assert.IsTrue(TronTestAccountCollection.TestMain.Address.Equals(address, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}

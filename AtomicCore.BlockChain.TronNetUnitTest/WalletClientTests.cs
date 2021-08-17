c144296f4a7cab730ce6ea15c6d4b5ff2fff1f6a
adu <adu@adu.com>
2021-08-17 20:19:09 +0800

    adds block extension method

---
+++ AtomicCore.BlockChain.TronNetUnitTest/WalletClientTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore.BlockChain.TronNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+
+namespace AtomicCore.BlockChain.TronNet.Tests
+{
+    [TestClass()]
+    public class WalletClientTests
+    {
+        private readonly TronTestRecord _record;
+        private readonly IWalletClient _wallet;
+        private readonly Wallet.WalletClient _walletProtocol;
+
+        public WalletClientTests()
+        {
+            _record = TronTestServiceExtension.GetMainRecord();
+            _wallet = _record.TronClient.GetWallet();
+            _walletProtocol = _wallet.GetProtocol();
+        }
+
+        /// <summary>
+        /// Get Block By Number 
+        /// </summary>
+        [TestMethod()]
+        public void GetBlockByNumber()
+        {
+            string blockID = "0000000001f618ad6a2d6492db91395ee6cb9c1ea8c4a38c456aa3aa57b592e5";
+            long blockNumber = 32905389;
+
+            Block block = _walletProtocol.GetBlockByNum(new NumberMessage()
+            {
+                Num = blockNumber
+            }, headers: _wallet.GetHeaders());
+
+            string currentBlockHash = block.GetBlockHash();
+
+            Assert.IsNull(blockID.Equals(currentBlockHash, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}

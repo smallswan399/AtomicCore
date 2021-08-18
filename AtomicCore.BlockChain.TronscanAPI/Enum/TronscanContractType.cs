9f182f9cc535218cc63f329718035a998f6c6adc
adu <adu@adu.com>
2021-08-18 14:42:22 +0800

    adds

---
+++ AtomicCore.BlockChain.TronscanAPI/Enum/TronscanContractType.cs
@@ -0,0 +1,15 @@
+﻿//namespace AtomicCore.BlockChain.TronscanAPI
+//{
+//    /// <summary>
+//    /// Tronscan Contract Type
+//    /// </summary>
+//    public enum TronscanContractType
+//    {
+//        /// <summary>
+//        /// Trx
+//        /// </summary>
+//        Trx = 1,
+
+
+//    }
+//}

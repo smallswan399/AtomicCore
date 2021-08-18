fd970d9224d5b3418172925a75aba6fb3ce469b2
adu <adu@adu.com>
2021-08-18 18:00:40 +0800

    adds

---
+++ AtomicCore.BlockChain.TronscanAPI/JsonResult/Contract/TronContractDataDetailsJson.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AtomicCore.BlockChain.TronscanAPI
+{
+    /// <summary>
+    /// Tron Contract Data Details Json
+    /// </summary>
+    public class TronContractDataDetailsJson : TronContractDataJson
+    {
+        /// <summary>
+        /// tokenInfo
+        /// </summary>
+        [JsonProperty("tokenInfo")]
+        public TronTokenBasicJson TokenInfo { get; set; }
+    }
+}

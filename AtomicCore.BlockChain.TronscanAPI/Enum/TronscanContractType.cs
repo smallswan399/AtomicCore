10f9e3e628dd623d2f2fd2c7dd98d46e62898c55
adu <adu@adu.com>
2021-08-19 20:20:40 +0800

    adds

--- AtomicCore.BlockChain.TronscanAPI/Enum/TronscanContractType.cs
+++ AtomicCore.BlockChain.TronscanAPI/Enum/TronscanContractType.cs
@@ -6,34 +6,104 @@
     public enum TronscanContractType
     {
         /// <summary>
+        /// Account Create Contract
+        /// </summary>
+        AccountCreateContract = 0,
+
+        /// <summary>
         /// Trx Transfer
         /// </summary>
-        TrxTransfer = 1,
+        TransferContract = 1,
 
         /// <summary>
         /// Trc10 Transfer(Other Trc10 Token)
         /// </summary>
-        Trc10Transfer = 2,
+        TransferAssetContract = 2,
 
         /// <summary>
-        /// vote
+        /// Vote Asset Contract
         /// </summary>
-        Vote = 4,
+        VoteAssetContract = 3,
 
         /// <summary>
-        /// lock trx assets for get bandwidth and energy
+        /// Vote Witness Contract
         /// </summary>
-        LockTrxAssets = 11,
+        VoteWitnessContract = 4,
 
         /// <summary>
-        /// Unlock Trx Assets
+        /// WitnessCreateContract
         /// </summary>
-        UnlockTrxAssets = 12,
+        WitnessCreateContract = 5,
 
         /// <summary>
-        /// Trc10 withdrawal income
+        /// AssetIssueContract
         /// </summary>
-        WithdrawalIncome = 13,
+        AssetIssueContract = 6,
+
+        /// <summary>
+        /// WitnessUpdateContract
+        /// </summary>
+        WitnessUpdateContract = 8,
+
+        /// <summary>
+        /// ParticipateAssetIssueContract
+        /// </summary>
+        ParticipateAssetIssueContract = 9,
+
+        /// <summary>
+        /// AccountUpdateContract
+        /// </summary>
+        AccountUpdateContract = 10,
+
+        /// <summary>
+        /// FreezeBalanceContract
+        /// </summary>
+        FreezeBalanceContract = 11,
+
+        /// <summary>
+        /// UnfreezeBalanceContract
+        /// </summary>
+        UnfreezeBalanceContract = 12,
+
+        /// <summary>
+        /// WithdrawBalanceContract
+        /// </summary>
+        WithdrawBalanceContract = 13,
+
+        /// <summary>
+        /// UnfreezeAssetContract
+        /// </summary>
+        UnfreezeAssetContract = 14,
+
+        /// <summary>
+        /// UpdateAssetContract
+        /// </summary>
+        UpdateAssetContract = 15,
+
+        /// <summary>
+        /// ProposalCreateContract
+        /// </summary>
+        ProposalCreateContract = 16,
+
+        /// <summary>
+        /// ProposalApproveContract
+        /// </summary>
+        ProposalApproveContract = 17,
+
+        /// <summary>
+        /// ProposalDeleteContract
+        /// </summary>
+        ProposalDeleteContract = 18,
+
+        /// <summary>
+        /// SetAccountIdContract
+        /// </summary>
+        SetAccountIdContract = 19,
+
+        /// <summary>
+        /// CustomContract
+        /// </summary>
+        CustomContract = 20,
 
         /// <summary>
         /// Create Smart Contract
@@ -44,5 +114,70 @@
         /// Trigger Smart Contract(eg:Trc20 && 721)
         /// </summary>
         TriggerSmartContract = 31,
+
+        /// <summary>
+        /// GetContract
+        /// </summary>
+        GetContract = 32,
+
+        /// <summary>
+        /// UpdateSettingContract
+        /// </summary>
+        UpdateSettingContract = 33,
+
+        /// <summary>
+        /// ExchangeCreateContract
+        /// </summary>
+        ExchangeCreateContract = 41,
+
+        /// <summary>
+        /// ExchangeInjectContract
+        /// </summary>
+        ExchangeInjectContract = 42,
+
+        /// <summary>
+        /// ExchangeWithdrawContract
+        /// </summary>
+        ExchangeWithdrawContract = 43,
+
+        /// <summary>
+        /// ExchangeTransactionContract
+        /// </summary>
+        ExchangeTransactionContract = 44,
+
+        /// <summary>
+        /// UpdateEnergyLimitContract
+        /// </summary>
+        UpdateEnergyLimitContract = 45,
+
+        /// <summary>
+        /// AccountPermissionUpdateContract
+        /// </summary>
+        AccountPermissionUpdateContract = 46,
+
+        /// <summary>
+        /// ClearABIContract
+        /// </summary>
+        ClearABIContract = 48,
+
+        /// <summary>
+        /// UpdateBrokerageContract
+        /// </summary>
+        UpdateBrokerageContract = 49,
+
+        /// <summary>
+        /// ShieldedTransferContract
+        /// </summary>
+        ShieldedTransferContract = 51,
+
+        /// <summary>
+        /// MarketSellAssetContract
+        /// </summary>
+        MarketSellAssetContract = 52,
+
+        /// <summary>
+        /// MarketCancelOrderContract
+        /// </summary>
+        MarketCancelOrderContract = 53
     }
 }

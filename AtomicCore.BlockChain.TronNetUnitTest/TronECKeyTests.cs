a0e3c7b4692369d1c41f18728fd75d33f1e89dea
adu <adu@adu.com>
2021-08-20 17:06:04 +0800

    add unit test

--- AtomicCore.BlockChain.TronNetUnitTest/TronECKeyTests.cs
+++ AtomicCore.BlockChain.TronNetUnitTest/TronECKeyTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AtomicCore.BlockChain.TronNet.Tests
 {
@@ -20,6 +21,18 @@ namespace AtomicCore.BlockChain.TronNet.Tests
             string address = mainKey.GetPublicAddress();
 
             Assert.IsTrue(TronTestAccountCollection.TestMain.Address.Equals(address, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [TestMethod()]
+        public void ConvertToTronAddressTest()
+        {
+            string date = "a9059cbb0000000000000000000000000e36621fbc6c3df109e8125282c84343f056420e00000000000000000000000000000000000000000000000000000000004c4b40";
+
+            string eth_address = string.Format("0x{0}", Regex.Replace(date.Substring(8, 64), @"^0*", string.Empty));
+
+            string tronAddress = TronECKey.ConvertToTronAddress(eth_address);
+
+            Assert.IsTrue("TBGMcMy84rxZijiQyjMDEJddMGgGoDQGH5".Equals(tronAddress, StringComparison.OrdinalIgnoreCase));
         }
     }
 }

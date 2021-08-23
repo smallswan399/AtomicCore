b71399f21886d03129c36355465c0887eabab839
adu <adu@adu.com>
2021-08-24 00:20:10 +0800

    adds

---
+++ AtomicCore.BlockChain.TronNet/RestAPI/Interface/ITronTransactionsRestAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.BlockChain.TronNet
+{
+    /// <summary>
+    /// Tron Transaction Rest API
+    /// </summary>
+    public interface ITronTransactionsRestAPI
+    {
+        void GetTransactionSign();
+
+        void BroadcastTransaction();
+
+        void BroadcastHex();
+
+        void EasyTransfer();
+
+        void EasyTransferByPrivate();
+
+        void CreateTransaction(string ownerAddress, string toAddress, ulong amount, int? permissionID, bool? visible);
+    }
+}
---
+++ AtomicCore.BlockChain.TronNet/RestAPI/RestJson/Transaction/TronCreateTransactionRawDataRestJson.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.BlockChain.TronNet
+{
+    /// <summary>
+    /// Tron Create Transaction RawData Rest Json
+    /// </summary>
+    public class TronCreateTransactionRawDataRestJson
+    {
+        /// <summary>
+        /// contract
+        /// </summary>
+        [JsonProperty("contract")]
+        public object[] Contract { get; set; }
+
+        /// <summary>
+        /// ref_block_bytes
+        /// </summary>
+        [JsonProperty("ref_block_bytes")]
+        public string RefBlockBytes { get; set; }
+
+        /// <summary>
+        /// ref_block_hash
+        /// </summary>
+        [JsonProperty("ref_block_hash")]
+        public string RefBlockHash { get; set; }
+
+        /// <summary>
+        /// expiration
+        /// </summary>
+        [JsonProperty("expiration"), JsonConverter(typeof(BizTronNetULongJsonConverter))]
+        public ulong Expiration { get; set; }
+
+        /// <summary>
+        /// timestamp
+        /// </summary>
+        [JsonProperty("timestamp"), JsonConverter(typeof(BizTronNetULongJsonConverter))]
+        public ulong Timestamp { get; set; }
+    }
+}

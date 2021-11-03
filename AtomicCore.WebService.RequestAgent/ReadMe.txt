7928f9d336a10f8cacb5e81888a1001c29653578
adu <adu@adu.com>
2021-11-04 00:06:20 +0800

    adds

---
+++ AtomicCore.WebService.RequestAgent/ReadMe.txt
@@ -0,0 +1,3 @@
+﻿docker build -t 13871772983/atomiccore.webservice.requestagent .
+
+docker push 13871772983/atomiccore.webservice.requestagent
--- AtomicCore.WebService.RequestAgent/Startup.cs
+++ AtomicCore.WebService.RequestAgent/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Encodings.Web;

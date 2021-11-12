17248252779eb927e94cf85b55e6defe6c54a236
adu <adu@adu.com>
2021-11-12 19:29:19 +0800

    adds

--- AtomicCore.Integration.MysqlDbProvider/nuget.bat
+++ AtomicCore.Integration.MysqlDbProvider/nuget.bat
@@ -10,6 +10,6 @@ pause
 :: %%f 是一个变量,类似于迭代器,但是这个变量只能由一个字母组成,前面带上%%
 :: 括号中是通配符,可以指定后缀名,*.*表示所有文件
 for %%f in (*.nupkg) do ( 
-	cmd start /k "dotnet nuget push %%f --api-key oy2g6uhfai675jvyvbkauroovhrl3xrethti46nj3i2mya --source https://api.nuget.org/v3/index.json"
+	cmd start /k "dotnet nuget push %%f --api-key %Atomic-Master-2.0-PROD% --source https://api.nuget.org/v3/index.json"
 )
 pause

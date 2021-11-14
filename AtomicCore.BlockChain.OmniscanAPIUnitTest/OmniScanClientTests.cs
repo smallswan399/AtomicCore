e9b7cc8d9365bc040de77000cfb23c93b06f2929
adu <adu@adu.com>
2021-11-14 20:16:45 +0800

    adds

---
+++ AtomicCore.BlockChain.OmniscanAPIUnitTest/OmniScanClientTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore.BlockChain.OmniscanAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.BlockChain.OmniscanAPI.Tests
+{
+    [TestClass()]
+    public class OmniScanClientTests
+    {
+        [TestMethod()]
+        public void GetAddressV2Test()
+        {
+            IOmniScanClient client = new OmniScanClient();
+            var result = client.GetAddressV2("1FoWyxwPXuj4C6abqwhjDWdz6D4PZgYRjA");
+
+            Assert.IsTrue(result.Count > 0);
+        }
+    }
+}

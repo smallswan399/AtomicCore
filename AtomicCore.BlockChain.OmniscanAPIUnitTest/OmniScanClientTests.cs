c08c53d0bd50b9250cd3487c034e1857c3ff1246
adu <adu@adu.com>
2021-11-22 15:49:22 +0800

    adds

--- AtomicCore.BlockChain.OmniscanAPIUnitTest/OmniScanClientTests.cs
+++ AtomicCore.BlockChain.OmniscanAPIUnitTest/OmniScanClientTests.cs
@@ -154,5 +154,23 @@ namespace AtomicCore.BlockChain.OmniscanAPI.Tests
 
             Assert.IsTrue(null != result);
         }
+
+        [TestMethod()]
+        public void PushTxTest()
+        {
+            IOmniScanClient client = new OmniScanClient();
+            var result = client.PushTx("01000000010b7ad3946c6fcd15d39fd4312c4f8dfd68c51a61071c2575cfb0722c4403eef1000000008a47304402206fe4eabf81cc4c6c3cee3dedc6ea9be46bb685c1b61da577bb9aead8188b80b702201404284c52ad2e857bd3931fb0e1646575d1e72c93b2cdcd80a7e0c7bee1c191014104ad90e5b6bc86b3ec7fac2c5fbda7423fc8ef0d58df594c773fa05e2c281b2bfe877677c668bd13603944e34f4818ee03cadd81a88542b8b4d5431264180e2c28ffffffff0470170000000000001976a9143b0000000000000001000000009d828d9600000088ac70170000000000001976a9143c791cc99255509d85788e2bf0e0f6e8b389b3cf88ac40355d5f000000001976a914946cb2e08075bcbaf157e47bcb67eb2b2339d24288ac70170000000000001976a914946cb2e08075bcbaf157e47bcb67eb2b2339d24288ac00000000");
+
+            Assert.IsTrue(null != result);
+        }
+
+        [TestMethod()]
+        public void GetTxTest()
+        {
+            IOmniScanClient client = new OmniScanClient();
+            var result = client.GetTx("e0e3749f4855c341b5139cdcbb4c6b492fcc09c49021b8b15462872b4ba69d1b");
+
+            Assert.IsTrue(null != result);
+        }
     }
 }

9b73701d8b21b51646c58df441f726c1f3a28bdc
adu <adu@adu.com>
2021-12-08 01:28:44 +0800

    adds

---
+++ AtomicCore.BlockChain.ExplorerAPIUnitTest/BtcExplorerClientTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore.BlockChain.ExplorerAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.BlockChain.ExplorerAPI.Tests
+{
+    [TestClass()]
+    public class BtcExplorerClientTests
+    {
+        [TestMethod()]
+        public void GetAddressBalanceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAddressTxsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

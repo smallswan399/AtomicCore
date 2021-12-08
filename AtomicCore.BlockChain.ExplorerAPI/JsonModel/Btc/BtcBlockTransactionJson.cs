d3fbd681bb357b828872b4dc784f89859b634b2e
adu <adu@adu.com>
2021-12-08 17:28:22 +0800

    adds

--- AtomicCore.BlockChain.ExplorerAPI/JsonModel/Btc/BtcBlockTransactionJson.cs
+++ AtomicCore.BlockChain.ExplorerAPI/JsonModel/Btc/BtcBlockTransactionJson.cs
@@ -19,31 +19,71 @@ namespace AtomicCore.BlockChain.ExplorerAPI
         [JsonProperty("ver")]
         public int TxVersion { get; set; }
 
-        public int vin_sz { get; set; }
+        /// <summary>
+        /// vin_sz
+        /// </summary>
+        [JsonProperty("vin_sz")]
+        public int TxVinCount { get; set; }
 
-        public int vout_sz { get; set; }
+        /// <summary>
+        /// vout_sz
+        /// </summary>
+        [JsonProperty("vout_sz")]
+        public int TxVoutCount { get; set; }
 
-        public int size { get; set; }
+        /// <summary>
+        /// size
+        /// </summary>
+        [JsonProperty("size")]
+        public int TxSize { get; set; }
 
-        public ulong fee { get; set; }
+        /// <summary>
+        /// fee
+        /// </summary>
+        [JsonProperty("Fee")]
+        public ulong TxFee { get; set; }
 
-        /////// <summary>
-        /////// relayed_by
-        /////// </summary>
-        ////[JsonProperty("relayed_by")]
-        ////public string RelayedBy { get; set; }
+        /// <summary>
+        /// relayed_by
+        /// </summary>
+        [JsonProperty("relayed_by")]
+        public string RelayedBy { get; set; }
 
-        public ulong lock_time { get; set; }
+        /// <summary>
+        /// lock_time
+        /// </summary>
+        [JsonProperty("lock_time")]
+        public ulong TxLockTime { get; set; }
 
-        public ulong tx_index { get; set; }
+        /// <summary>
+        /// tx_index
+        /// </summary>
+        [JsonProperty("tx_index")]
+        public ulong TxIndex { get; set; }
 
-        public bool double_spend { get; set; }
+        /// <summary>
+        /// double_spend
+        /// </summary>
+        [JsonProperty("double_spend")]
+        public bool TxDoubleSpend { get; set; }
 
-        public ulong times { get; set; }
+        /// <summary>
+        /// times
+        /// </summary>
+        [JsonProperty("times")]
+        public ulong TxTimestamp { get; set; }
 
-        public ulong block_index { get; set; }
+        /// <summary>
+        /// block_index
+        /// </summary>
+        [JsonProperty("block_index")]
+        public ulong BlockIndex { get; set; }
 
-        public ulong block_height { get; set; }
+        /// <summary>
+        /// block_height
+        /// </summary>
+        [JsonProperty("block_height")]
+        public ulong BlockHeight { get; set; }
 
         /// <summary>
         /// tx vins

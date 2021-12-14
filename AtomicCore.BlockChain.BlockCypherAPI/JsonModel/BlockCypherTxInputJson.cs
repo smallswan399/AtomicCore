3c022352ab6ba269176281fc66c9b5c3464f1123
adu <adu@adu.com>
2021-12-14 23:09:26 +0800

    adds

---
+++ AtomicCore.BlockChain.BlockCypherAPI/JsonModel/BlockCypherTxInputJson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// https://www.blockcypher.com/dev/bitcoin/?shell#txinput
+    /// </summary>
+    public class BlockCypherTxInputJson
+    {
+    }
+}
---
+++ AtomicCore.BlockChain.BlockCypherAPI/JsonModel/BlockCypherTxJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// block cypher tx json
+    /// https://www.blockcypher.com/dev/bitcoin/?shell#tx
+    /// </summary>
+    public class BlockCypherTxJson
+    {
+    }
+}

03d09168567c2a70a226a6c9bf43c79334240593
adu <adu@adu.com>
2021-12-14 15:17:45 +0800

    adds

---
+++ AtomicCore.BlockChain.BlockCypherAPI/BlockCypherSrvAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// BlockCypher Service
+    /// </summary>
+    public class BlockCypherSrvAPI : IBlockCypherAPI
+    {
+    }
+}
---
+++ AtomicCore.BlockChain.BlockCypherAPI/IBlockCypherAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// BlockCypherAPI Interface
+    /// </summary>
+    public interface IBlockCypherAPI
+    {
+    }
+}

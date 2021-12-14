6c4ffec58603611930da84c1efd07f0df68a6ab8
adu <adu@adu.com>
2021-12-14 23:17:28 +0800

    adds

---
+++ AtomicCore.BlockChain.BlockCypherAPI/JsonModel/BlockCypherAddressForwardCallbackJson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// https://www.blockcypher.com/dev/bitcoin/?shell#addressforwardcallback
+    /// </summary>
+    public class BlockCypherAddressForwardCallbackJson
+    {
+    }
+}
---
+++ AtomicCore.BlockChain.BlockCypherAPI/JsonModel/BlockCypherOAPIssueJson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// https://www.blockcypher.com/dev/bitcoin/?shell#oapissue
+    /// </summary>
+    public class BlockCypherOAPIssueJson
+    {
+    }
+}

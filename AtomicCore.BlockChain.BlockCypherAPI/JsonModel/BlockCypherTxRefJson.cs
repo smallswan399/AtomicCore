ffc72e0edabddbef2af9af115ab94e175ccf539c
adu <adu@adu.com>
2021-12-14 23:13:06 +0800

    adds

---
+++ AtomicCore.BlockChain.BlockCypherAPI/JsonModel/BlockCypherTxRefJson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// https://www.blockcypher.com/dev/bitcoin/?shell#txref
+    /// </summary>
+    public class BlockCypherTxRefJson
+    {
+    }
+}

455c2ce971c60c78484f2b29ff452c01c830f642
stone <stone@DESKTOP-5ALU65R>
2021-12-18 15:34:39 +0800

    ai ni

---
+++ AtomicCore.BlockChain.BlockCypherAPI/JsonModel/BlockCypherListWalletsJson.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BlockCypherAPI
+{
+    /// <summary>
+    /// https://www.blockcypher.com/dev/bitcoin/?shell#list-wallets-endpoint
+    /// List Wallets Endpoint 返回
+    /// </summary>
+    public class BlockCypherListWalletsJson
+    {
+        [JsonProperty("wallet_names")]
+        public string WalletNames { get; set; }
+    }
+}

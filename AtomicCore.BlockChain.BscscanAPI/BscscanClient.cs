5b403e3b5cc81fe6c95a0713665966404d91f2f9
adu <adu@adu.com>
2021-12-18 16:50:02 +0800

    adds

---
+++ AtomicCore.BlockChain.BscscanAPI/BscscanClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BscscanAPI
+{
+    /// <summary>
+    /// bscscan client service
+    /// </summary>
+    public class BscscanClient : IBscscanClient
+    {
+
+
+    }
+}
---
+++ AtomicCore.BlockChain.BscscanAPI/IBscscanClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomicCore.BlockChain.BscscanAPI
+{
+    /// <summary>
+    /// bscscan client interface
+    /// </summary>
+    public interface IBscscanClient
+    {
+    }
+}

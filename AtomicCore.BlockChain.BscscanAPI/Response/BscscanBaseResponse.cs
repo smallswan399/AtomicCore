9336ec469457e9386644bb51cca781e37c5c7e1f
adu <adu@adu.com>
2021-12-18 15:47:19 +0800

    adds

---
+++ AtomicCore.BlockChain.BscscanAPI/Response/BscscanBaseResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace AtomicCore.BlockChain.BscscanAPI
+{
+    /// <summary>
+    /// bsc api response
+    /// </summary>
+    public class BscscanBaseResponse
+    {
+        /// <summary>
+        /// 消息状态(1 true)
+        /// </summary>
+        [JsonProperty("status")]
+        public BscscanJsonStatus Status { get; set; }
+
+        /// <summary>
+        /// 消息信息
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}

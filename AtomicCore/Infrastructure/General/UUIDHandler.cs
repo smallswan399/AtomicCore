a4b1e6cc71c7d1f52dfb23e9dd7229d397444151
danny <171331668@qq.com>
2022-02-20 16:20:09 +0800

    adds

--- AtomicCore/Infrastructure/General/UUIDHandler.cs
+++ AtomicCore/Infrastructure/General/UUIDHandler.cs
@@ -16,7 +16,7 @@ namespace AtomicCore
             long i = 1;
 
             foreach (byte b in Guid.NewGuid().ToByteArray())
-                i *= ((int)b + 1);
+                i *= b + 1;
 
             return string.Format("{0:x}", i - DateTime.Now.Ticks);
         }

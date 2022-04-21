ec630aaeddf3cc13676bed0cbb7df83bdc798617
danny <171331668@qq.com>
2022-04-21 18:46:31 +0800

    adds

---
+++ AtomicCore.BlockChain.EtherscanAPI/Services/IEtherProxy.cs
@@ -0,0 +1,9 @@
+﻿namespace AtomicCore.BlockChain.EtherscanAPI
+{
+    /// <summary>
+    /// https://docs.etherscan.io/api-endpoints/geth-parity-proxy
+    /// </summary>
+    public interface IEtherProxy
+    {
+    }
+}

4103500cab1c25146534eb2fe9731ad56ce0c987
danny <171331668@qq.com>
2022-04-29 02:37:45 +0800

    adds

--- AtomicCore/Infrastructure/Cryptography/Impls/AesSymmetricAlgorithm.cs
+++ AtomicCore/Infrastructure/Cryptography/Impls/AesSymmetricAlgorithm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -16,11 +15,6 @@ namespace AtomicCore
         /// default key
         /// </summary>
         private const string def_consultKey = "gotogether";
-
-        /// <summary>
-        /// key min length
-        /// </summary>
-        private const int c_min_keyLenght = 16;
 
         #endregion
 
@@ -47,7 +41,7 @@ namespace AtomicCore
                             confKey = def_consultKey;
                     }
 
-                    this._algorithmKey = MD5Handler.Generate(confKey, true).Top(c_min_keyLenght);
+                    this._algorithmKey = confKey;
                 }
 
                 return this._algorithmKey;
@@ -72,40 +66,42 @@ namespace AtomicCore
         /// <returns></returns>
         public string Encrypt(string origialText, params object[] argumentParam)
         {
-            string result = string.Empty;
+            #region 基础判断
 
-            #region 加密参数选取
+            if (string.IsNullOrEmpty(origialText))
+                throw new ArgumentNullException(nameof(origialText));
+
+            #endregion
+
+            #region 计算KEY
 
             string key_str;
             if (null == argumentParam || argumentParam.Length <= 0)
                 key_str = this.AlgorithmKey;
             else
-            {
-                if (argumentParam.Length == 1)
-                    key_str = argumentParam.First().ToString();
-                else
-                    key_str = this.AlgorithmKey;
+                if (argumentParam[0] is string algorithmKey)
+                key_str = algorithmKey;
+            else
+                key_str = this.AlgorithmKey;
 
-                if (key_str.Length < c_min_keyLenght)
-                    key_str = key_str.PadRight(c_min_keyLenght, char.MinValue);
-            }
+            var key = Encoding.UTF8.GetBytes(key_str);
 
             #endregion
 
             #region 执行AES加密 
 
-            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key_str);
-            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(origialText);
+            string result = string.Empty;
+            byte[] plainText = Encoding.UTF8.GetBytes(origialText);
 
             using (var rDel = new RijndaelManaged())
             {
                 rDel.Mode = CipherMode.ECB;
                 rDel.Padding = PaddingMode.PKCS7;
-                rDel.Key = keyArray;
+                rDel.Key = key;
 
                 byte[] cipherBytes;
                 using (ICryptoTransform transform = rDel.CreateEncryptor())
-                    cipherBytes = transform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                    cipherBytes = transform.TransformFinalBlock(plainText, 0, plainText.Length);
 
                 result = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
             }
@@ -123,40 +119,44 @@ namespace AtomicCore
         /// <returns></returns>
         public string Decrypt(string ciphertext, params object[] argumentParam)
         {
-            string result = string.Empty;
+            #region 基础判断
 
-            #region 解密参数选取
+            if (string.IsNullOrEmpty(ciphertext))
+                throw new ArgumentNullException(nameof(ciphertext));
+            if (!Base64Handler.IsBase64Format(ciphertext))
+                throw new ArgumentException($"'{ciphertext}' is not a valid base64!");
+
+            #endregion
+
+            #region 计算KEY
 
             string key_str;
             if (null == argumentParam || argumentParam.Length <= 0)
                 key_str = this.AlgorithmKey;
             else
-            {
-                if (argumentParam.Length == 1)
-                    key_str = argumentParam.First().ToString();
-                else
-                    key_str = this.AlgorithmKey;
+                if (argumentParam[0] is string algorithmKey)
+                key_str = algorithmKey;
+            else
+                key_str = this.AlgorithmKey;
 
-                if (key_str.Length < c_min_keyLenght)
-                    key_str = key_str.PadRight(c_min_keyLenght, char.MinValue);
-            }
+            var key = Encoding.UTF8.GetBytes(key_str);
 
             #endregion
 
             #region 执行AES解密
 
-            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key_str);
-            byte[] toEncryptArray = Convert.FromBase64String(ciphertext);
+            string result = string.Empty;
+            byte[] cipher_bytes = Convert.FromBase64String(ciphertext);
 
             using (var rDel = new RijndaelManaged())
             {
                 rDel.Mode = CipherMode.ECB;
                 rDel.Padding = PaddingMode.PKCS7;
-                rDel.Key = keyArray;
+                rDel.Key = key;
 
                 byte[] plainText;
                 using (ICryptoTransform transform = rDel.CreateDecryptor())
-                    plainText = transform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                    plainText = transform.TransformFinalBlock(cipher_bytes, 0, cipher_bytes.Length);
 
                 result = Encoding.UTF8.GetString(plainText);
             }

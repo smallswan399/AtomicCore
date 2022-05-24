4104ddc0abe1d969a2f0a32d8499bb2e5fed5c66
danny <171331668@qq.com>
2022-05-24 23:33:17 +0800

    adds

---
+++ AtomicCore.BlockChain.TronNet/Services/Grid/Models/Transaction/TronGridTransactionParameter.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace AtomicCore.BlockChain.TronNet
+{
+    /// <summary>
+    /// TronGridTransactionParameter
+    /// </summary>
+    public class TronGridTransactionParameter
+    {
+        /// <summary>
+        /// type_url
+        /// </summary>
+        [JsonProperty("type_url")]
+        public string TypeUrl { get; set; }
+
+        /// <summary>
+        /// value
+        /// </summary>
+        [JsonProperty("value")]
+        public object Value { get; set; }
+    }
+}

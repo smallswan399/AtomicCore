12550e7385eb9abcd6bccd7edbf8ce29245459fd
danny <171331668@qq.com>
2022-05-29 16:00:45 +0800

    adds

---
+++ AtomicCore.BlockChain.TronNet/Services/Grid/Models/Transaction/ParamValue/TronGridTransferAssetContractInfo.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace AtomicCore.BlockChain.TronNet
+{
+    /// <summary>
+    /// TronGrid Transfer Asset Contract Info
+    /// </summary>
+    [TronNetParamValue(TronNetContractType.TransferAssetContract)]
+    public class TronGridTransferAssetContractInfo : TronGridTransferContractInfo
+    {
+        #region Propertys
+
+        /// <summary>
+        /// contract_address
+        /// </summary>
+        [JsonProperty("asset_name")]
+        public string AssetName { get; set; }
+
+        #endregion
+    }
+}
